dc9583bc8877a9ad38b578e963d63f8afbb36b04
CarmenMerino <carmenmerino2202@gmail.com>
2022-04-05 10:54:23 -0500

    cambioss

--- src/database.js
+++ src/database.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 // eslint-disable-next-line import/no-unresolved
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
-=======
-/* eslint-disable no-console */
-/* eslint-disable import/no-unresolved */
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-} from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
->>>>>>> 54cbaae64cae8eeb5bc0faf8a4954490837d930c
 import { app } from './main.js';
 
 const db = getFirestore(app);
@@ -23,12 +12,3 @@ export async function insertData(publication) {
     console.error('Error adding document: ', e);
   }
 }
-<<<<<<< HEAD
-=======
-
-// Esta función se encarga de traer los datos de la colección 'publications' de firestore:
-export async function getData() {
-  const querySnapshot = await getDocs(collection(db, 'publications'));
-  return querySnapshot;
-}
->>>>>>> 54cbaae64cae8eeb5bc0faf8a4954490837d930c
